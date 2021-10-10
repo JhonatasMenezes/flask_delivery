8637875e2d488150b11083ac653f872efe537fb4
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-10-09 23:26:10 -0300

    Adicionando os primeiros contceitos de tempates para o projeto.

--- delivery/ext/site/main.py
+++ delivery/ext/site/main.py
@@ -1,8 +1,18 @@
-from flask import request, render_template
+from flask import render_template
 from flask import Blueprint
 
 bp = Blueprint("site", __name__)
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@bp.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/restaurantes")
+def restaurantes():
+    return render_template("restaurants.html")
